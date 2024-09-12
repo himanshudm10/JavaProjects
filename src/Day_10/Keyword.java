2b62a834e05b28015dbde3f92129e68f271eab27
himanshudm10 <himanshudma10@gmail.com>
2024-09-12 12:57:49 +0530

    Keywords

---
+++ src/Day_10/Keyword.java
@@ -0,0 +1,31 @@
+package Day_10;
+
+public class Keyword 
+{
+
+	int x,y; //class variable/instance variable
+	static int z=20;
+	
+	
+	void setData(int x, int y)   //local variable
+	{
+		this.x=x;
+		this.y=y;
+	}
+	
+	void display()
+	{
+		System.out.println(x+" "+y);
+	}
+
+	public static void main(String[] args)
+	{
+		Keyword x= new Keyword();
+		System.out.println(z);
+		x.setData(15,56);
+		x.display();
+	}
+
+}
+
+
